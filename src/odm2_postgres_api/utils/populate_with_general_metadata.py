27bb621315a6b4181f4ccab0f57853df6ab07f86
RmStorm <roaldstorm@gmail.com>
2020-07-10 17:40:03 +0200

    Add metadata for begroing indices, closes #24

--- src/odm2_postgres_api/utils/populate_with_general_metadata.py
+++ src/odm2_postgres_api/utils/populate_with_general_metadata.py
@@ -81,6 +81,100 @@ processing_levels = {'processing_levels': [{
                    "isotope analysis."
 }]}
 
+begroing_indices = [{
+    "name": "PIT",
+    "term": "PIT (Periphyton Index of Trophic status)",
+    "definition": "An index to keep track of water quality as described here: https://link.springer.com/article/10.1007/s10750-011-0614-7",  # nopep8
+    "category": "Biota",
+    "controlled_vocabulary_table_name": "cv_variablename"
+}, {
+    "name": "AIP",
+    "term": "AIP (Acidification Index Periphyton)",
+    "definition": "An index to keep track of water quality as described here: https://www.sciencedirect.com/science/article/abs/pii/S1470160X09000491",  # nopep8
+    "category": "Biota",
+    "controlled_vocabulary_table_name": "cv_variablename"
+}, {
+    "name": "HBI",
+    "term": "HBI (heterotrof begroingsindeks)",
+    "definition": "An index to keep track of water quality that is based on heterotroph organisms",
+    "category": "Biota",
+    "controlled_vocabulary_table_name": "cv_variablename"
+}, {
+    "name": "HBI2",
+    "term": "HBI2 (heterotrof begroingsindeks 2)",
+    "definition": "An index to keep track of water quality that is based on heterotroph organisms version2",
+    "category": "Biota",
+    "controlled_vocabulary_table_name": "cv_variablename"
+}, {
+    "name": "PIT EQR",
+    "term": "PIT EQR (Periphyton Index of Trophic status, Ecological Quality Ratio)",
+    "definition": "An index to keep track of water quality as described here: https://link.springer.com/article/10.1007/s10750-011-0614-7",  # nopep8
+    "category": "Biota",
+    "controlled_vocabulary_table_name": "cv_variablename"
+}, {
+    "name": "AIP EQR",
+    "term": "AIP EQR (Acidification Index Periphyton, Ecological Quality Ratio)",
+    "definition": "An index to keep track of water quality as described here: https://www.sciencedirect.com/science/article/abs/pii/S1470160X09000491",  # nopep8
+    "category": "Biota",
+    "controlled_vocabulary_table_name": "cv_variablename"
+}, {
+    "name": "HBI EQR",
+    "term": "HBI EQR (heterotrof begroingsindeks, Ecological Quality Ratio)",
+    "definition": "An index to keep track of water quality that is based on heterotroph organisms",
+    "category": "Biota",
+    "controlled_vocabulary_table_name": "cv_variablename"
+}, {
+    "name": "HBI2 EQR",
+    "term": "HBI2 EQR (heterotrof begroingsindeks 2, Ecological Quality Ratio)",
+    "definition": "An index to keep track of water quality that is based on heterotroph organisms version2",
+    "category": "Biota",
+    "controlled_vocabulary_table_name": "cv_variablename"
+}, {
+    "name": "PIT nEQR",
+    "term": "PIT nEQR (Periphyton Index of Trophic status, normalized Ecological Quality Ratio)",
+    "definition": "An index to keep track of water quality as described here: https://link.springer.com/article/10.1007/s10750-011-0614-7",  # nopep8
+    "category": "Biota",
+    "controlled_vocabulary_table_name": "cv_variablename"
+}, {
+    "name": "AIP nEQR",
+    "term": "AIP nEQR (Acidification Index Periphyton, normalized Ecological Quality Ratio)",
+    "definition": "An index to keep track of water quality as described here: https://www.sciencedirect.com/science/article/abs/pii/S1470160X09000491",  # nopep8
+    "category": "Biota",
+    "controlled_vocabulary_table_name": "cv_variablename"
+}, {
+    "name": "HBI nEQR",
+    "term": "HBI nEQR (heterotrof begroingsindeks, normalized Ecological Quality Ratio)",
+    "definition": "An index to keep track of water quality that is based on heterotroph organisms",
+    "category": "Biota",
+    "controlled_vocabulary_table_name": "cv_variablename"
+}, {
+    "name": "HBI2 nEQR",
+    "term": "HBI2 nEQR (heterotrof begroingsindeks 2, normalized Ecological Quality Ratio)",
+    "definition": "An index to keep track of water quality that is based on heterotroph organisms version2",
+    "category": "Biota",
+    "controlled_vocabulary_table_name": "cv_variablename"
+}]
+
+
+controlled_vocabularies = {'controlled_vocabularies': [{
+    "term": "trackSeriesCoverage",
+    "name": "Track series coverage",
+    "definition": "A series of ResultValues for a single Variable, measured with a moving platform or some sort of "
+                  "variable location, using a single Method, with specific Units, having a specific ProcessingLevel, "
+                  "and measured over time.",
+    "category": "Coverage",
+    "controlled_vocabulary_table_name": "cv_resulttype"
+}, *begroing_indices]}
+
+begroing_variables = [{
+    "variabletypecv": "Water quality",
+    "variablenamecv": index_dict["name"],
+    "variabledefinition": "water quality index",
+    "variablecode": f'begroing_{i+1}',  # The abundance variable down below is begroing_0
+    "nodatavalue": -9999
+} for i, index_dict in enumerate(begroing_indices)]
+
+# Most units taken from http://vocabulary.odm2.org/units/
 units_and_variables = {'units': [{
     "unitstypecv": "Salinity",
     "unitsabbreviation": "PSU",
@@ -155,6 +249,10 @@ units_and_variables = {'units': [{
     "unitstypecv": "Time",
     "unitsabbreviation": "s",
     "unitsname": "second"
+}, {
+    "unitstypecv": "Dimensionless",
+    "unitsabbreviation": "-",
+    "unitsname": "Dimensionless"
 }], 'variables': [{
     "variabletypecv": "Chemistry",
     "variablenamecv": "Salinity",
@@ -213,19 +311,11 @@ units_and_variables = {'units': [{
     "variabletypecv": "Biota",
     "variablenamecv": "Abundance",
     "variabledefinition": "This variable indicates the abundance of the taxon of the result",
-    "variablecode": "010",
+    "variablecode": "begroing_0",
     "nodatavalue": -9999
-}]}
+}, *begroing_variables]}
 
-other_stuff = {'controlled_vocabularies': {
-    "term": "trackSeriesCoverage",
-    "name": "Track series coverage",
-    "definition": "A series of ResultValues for a single Variable, measured with a moving platform or some sort of "
-                  "variable location, using a single Method, with specific Units, having a specific ProcessingLevel, "
-                  "and measured over time.",
-    "category": "Coverage",
-    "controlled_vocabulary_table_name": "cv_resulttype"
-}, 'methods': [{
+methods = {'methods': [{
     "methodtypecv": "Instrument deployment",
     "methodcode": "000",
     "methodname": "Deploy an Instrument",
@@ -267,19 +357,29 @@ other_stuff = {'controlled_vocabularies': {
     "methodname": "Kiselalger relative abundance",
     "methoddescription": "Relative abundance is the percent composition of an organism of a particular kind relative to the total number of organisms in the area. This observation has percentage as it's unit and it needs a classifying taxon in the result to make sense.",  # nopep8
     "organizationid": 1  # organization 1 is NIVA
+}, {
+    "methodtypecv": "Derivation",
+    "methodcode": "007",
+    "methodname": "Begroing Index Calculation",
+    "methoddescription": "Calculate an index value according to it's variable description, supported variable now are PIT, AIP, HBI, HBI2 and their normalized variants",  # nopep8
+    "organizationid": 1  # organization 1 is NIVA
 }]}
 
 
 def post_to_odm2_api(endpoint, data):
     response = requests.post('http://localhost:8701/' + endpoint, json=data)
-    response.raise_for_status()
+    try:
+        response.raise_for_status()
+    except Exception:
+        logging.error(data)
+        raise
     logging.info(f"Succesfully inserted into '{endpoint}': {response.json()}")
     return response.json()
 
 
 def main():
     setup_logging(plaintext=True)
-    data_sets = [people_and_organizations, processing_levels, units_and_variables, other_stuff]
+    data_sets = [people_and_organizations, processing_levels, controlled_vocabularies, units_and_variables, methods]
     for data_set in data_sets:
         for endpoint, data in data_set.items():
             if type(data) == list:
