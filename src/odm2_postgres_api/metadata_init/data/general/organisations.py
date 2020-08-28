101a1f796e7798c63621b35a601e9a1993f3c51f
Håkon Drolsum Røkenes <hakon.rokenes@niva.no>
2020-08-28 13:58:11 +0200

    streamlined metadata setup #49
    - refactored the script so that sequence of execution is a bit more
    readable. Split metadata setup into different files
    - init metadata on app startup. This ensures that it is automatically
    ran in all environments
    - write metadata directly to database during app startup instead of in
    its own process over http
    - added checks in write functions if row exists, we do nothing
      - NB: updates not supported!
    - dynamically setting niva org id in cases where it was hardcoded
    
    misc changes:
    
    - moved db_init and config to its own folder
    - differentiated cv create and response (cv create has CV_TABLE as part
    of it). Could consider to move this to pathparam as well?
    - moved begroing specific stuff to its own file
    - removed a lot of endpoints as these are only used for metadata setup
      - may consider to move them back in as endpoints in the future..
    - populating metadata as part of integration tests
    - setting search path as part of connection pool init
      - hopefully this fixes #56
    
    TODOS:
    
    -no way to update org, person, etc. as it is now. Only prevent
    duplicates

---
+++ src/odm2_postgres_api/metadata_init/data/general/organisations.py
@@ -0,0 +1,12 @@
+from odm2_postgres_api.schemas.schemas import OrganizationsCreate
+
+
+def niva_org() -> OrganizationsCreate:
+    org = {
+        "organizationtypecv": "Research institute",
+        "organizationcode": "niva-no",
+        "organizationname": "Norsk institutt for vannforskning",
+        "organizationdescription": "Norwegian institute for water research",
+        "organizationlink": "www.niva.no"
+    }
+    return OrganizationsCreate(**org)
